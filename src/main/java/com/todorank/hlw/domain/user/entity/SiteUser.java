883186b208318a27137f3951fe3d040a269ab690
Leesamgsoo <sslee3680035@gmail.com>
2024-10-02 12:34:14 +0900

    base entity

---
+++ src/main/java/com/todorank/hlw/domain/user/entity/SiteUser.java
@@ -0,0 +1,30 @@
+package com.todorank.hlw.domain.user.entity;
+
+import com.todorank.hlw.global.jpa.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SiteUser extends BaseEntity {
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String socialProvider;
+
+    @Column(columnDefinition = "text")
+    private String intro;
+}
