3fa6d7e8905436a97f1fe7ef14aa9e07fff7f8fb
EunKyu00 <ahz1105@naver.com>
2024-10-09 13:50:36 +0900

    todo-list 상세페이지

--- src/main/java/com/todorank/hlw/domain/home/MainController.java
+++ src/main/java/com/todorank/hlw/domain/home/MainController.java
@@ -1,6 +1,7 @@
 package com.todorank.hlw.domain.home;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -10,5 +11,9 @@ public class MainController {
     @ResponseBody
     public String index() {
         return "홈입니다.";
+    }
+    @GetMapping("/todo/detail")
+    public String detail(){
+        return "todo_list_detail";
     }
 }
