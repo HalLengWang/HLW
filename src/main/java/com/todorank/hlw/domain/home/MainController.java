6c94d4816e45441d504cf91925102b1133941bdd
taeho5825 <xogh3322@naver.comm>
2024-10-10 12:33:12 +0900

    C:/Program Files/Git/

--- src/main/java/com/todorank/hlw/domain/home/MainController.java
+++ src/main/java/com/todorank/hlw/domain/home/MainController.java
@@ -9,34 +9,4 @@ public class MainController {
     public String root() {
         return "redirect:/user/signup";
     }
-
-    @GetMapping("/login")
-    public String root1() {
-        return "login";
-    }
-
-    @GetMapping("/todo_card_read_create")
-    public String root2() {
-        return "todo_card_read_create";
-    }
-
-    @GetMapping("/signup")
-    public String root3() {
-        return "signup";
-    }
-
-    @GetMapping("/profile_read")
-    public String root4() {
-        return "profile_read";
-    }
-
-    @GetMapping("/profile_modify")
-    public String root5() {
-        return "profile_modify";
-    }
-
-    @GetMapping("/remembrance")
-    public String root6() {
-        return "remembrance";
-    }
 }
