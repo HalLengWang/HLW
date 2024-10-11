1951f348474bdc20f7971d81199bdb5c08ac875b
taeho5825 <xogh3322@naver.comm>
2024-10-11 10:07:53 +0900

    navbar

--- src/main/java/com/todorank/hlw/domain/home/MainController.java
+++ src/main/java/com/todorank/hlw/domain/home/MainController.java
@@ -9,24 +9,4 @@ public class MainController {
     public String root() {
         return "login_after";
     }
-
-    @GetMapping("/navbar")
-    public String root1() {
-        return "navbar";
-    }
-
-    @GetMapping("/login")
-    public String root2() {
-        return "login";
-    }
-
-    @GetMapping("/profile_read")
-    public String root3() {
-        return "profile_read";
-    }
-
-    @GetMapping("/todo_card_read_create")
-    public String root4() {
-        return "todo_card_read_create";
-    }
 }
