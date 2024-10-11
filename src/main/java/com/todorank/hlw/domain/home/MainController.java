fee3cbcb96f5f98c634270d424df2b69bfe551a7
EunKyu00 <ahz1105@naver.com>
2024-10-11 13:43:23 +0900

    login_after 검색기능, 버튼 수정

--- src/main/java/com/todorank/hlw/domain/home/MainController.java
+++ src/main/java/com/todorank/hlw/domain/home/MainController.java
@@ -9,4 +9,8 @@ public class MainController {
     public String root() {
         return "login_after";
     }
+    @GetMapping("/t")
+    public String remembrance(){
+        return "remembrance";
+    }
 }
