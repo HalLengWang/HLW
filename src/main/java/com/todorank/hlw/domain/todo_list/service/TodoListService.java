25ffc5b37e41b7f57bc0d3cb7215115407669e74
LeeSamgsoo <48269505+LeeSamgsoo@users.noreply.github.com>
2024-10-11 11:41:44 +0900

    Feature/lss (#34)
    * test file delete
    
    * html modify
    
    # Conflicts:
    #	src/main/resources/templates/todo_list.html
    #	src/main/resources/templates/todo_list_detail.html
    
    * 권한별로 접근성 구분작업

--- src/main/java/com/todorank/hlw/domain/todo_list/service/TodoListService.java
+++ src/main/java/com/todorank/hlw/domain/todo_list/service/TodoListService.java
@@ -4,8 +4,13 @@ import com.todorank.hlw.domain.todo_list.entity.TodoList;
 import com.todorank.hlw.domain.todo_list.repository.TodoListRepository;
 import com.todorank.hlw.domain.user.entity.SiteUser;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -15,5 +20,12 @@ public class TodoListService {
 
     public List<TodoList> getLists(SiteUser user) {
         return this.todoListRepository.findByUser(user);
+    }
+
+    public Page<TodoList> getPage(SiteUser user, int page) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createdDate"));
+        Pageable pageable = PageRequest.of(page, 5, Sort.by(sorts));
+        return this.todoListRepository.findByUser(user, pageable);
     }
 }
