5fd72d26ca77b06be41e1e0541e33072be2c41cd
taeho5825 <xogh3322@naver.comm>
2024-10-14 09:07:37 +0900

    remembrance modify

--- src/main/java/com/todorank/hlw/domain/home/MainController.java
+++ src/main/java/com/todorank/hlw/domain/home/MainController.java
@@ -5,8 +5,5 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-    @GetMapping("/")
-    public String root() {
-        return "login_after";
-    }
+
 }
