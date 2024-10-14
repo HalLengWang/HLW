f3bdbb7879865434d21f36169331464a3a5c2fd3
taeho5825 <xogh3322@naver.comm>
2024-10-14 11:19:02 +0900

    3

--- src/main/java/com/todorank/hlw/domain/home/MainController.java
+++ src/main/java/com/todorank/hlw/domain/home/MainController.java
@@ -1,13 +1,7 @@
 package com.todorank.hlw.domain.home;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-    @GetMapping("/todo_card_read_create_page")
-    public String root(){
-        return "/todo_card_read_create_page";
-    }
-
 }
