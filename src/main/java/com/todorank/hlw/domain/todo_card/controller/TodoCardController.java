11d121f069a7c1027d563fb6221b0fafab7f851b
Leesamgsoo <sslee3680035@gmail.com>
2024-10-15 13:48:04 +0900

    todo_card_show, html 진행도 표시부터 이어서 하기

--- src/main/java/com/todorank/hlw/domain/todo_card/controller/TodoCardController.java
+++ src/main/java/com/todorank/hlw/domain/todo_card/controller/TodoCardController.java
@@ -1,5 +1,6 @@
 package com.todorank.hlw.domain.todo_card.controller;
 
+import com.todorank.hlw.domain.todo_card.entity.TodoCard;
 import com.todorank.hlw.domain.todo_card.form.TodoCardForm;
 import com.todorank.hlw.domain.todo_card.service.TodoCardService;
 import com.todorank.hlw.domain.todo_list.DTO.TodoListDTO;
@@ -14,10 +15,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
@@ -64,5 +62,24 @@ public class TodoCardController {
                 todoCardForm.getEndDateTime(), todoCardForm.getCompletion(), todoCardForm.getExecution(),
                 todoType, todoList);
         return String.format("redirect:/todo_list/detail/%s", list_id);
+    }
+
+    @GetMapping("/detail/{id}")
+    public String todoCardDetail(TodoCardForm todoCardForm, @PathVariable(value = "id") Long cardId,
+                                 Model model) {
+        TodoCard todoCard = this.todoCardService.getCard(cardId);
+        List<TodoTypeList> typeList = this.todoTypeListService.getList();
+        if (todoCard == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "존재하지 않는 데이터입니다.");
+        }
+        model.addAttribute("todoTypeList", typeList);
+        todoCardForm.setExecution(todoCard.getExecution());
+        todoCardForm.setMemo(todoCard.getMemo());
+        todoCardForm.setCategory(todoCard.getTodoTypeList().getId());
+        todoCardForm.setTitle(todoCard.getTitle());
+        todoCardForm.setCompletion(todoCard.getCompletion());
+        todoCardForm.setEndDateTime(todoCard.getEndDateTime());
+        todoCardForm.setStartDateTime(todoCard.getStartDateTime());
+        return "todo_card_read_create_page";
     }
 }
