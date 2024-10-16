e50cb55b923626c6bb4cdcb3e55bb5ffd1edca47
LeeSamgsoo <48269505+LeeSamgsoo@users.noreply.github.com>
2024-10-16 13:44:18 +0900

    Feature/todo card show (#70)
    * test file delete
    
    * todo card show, html에 하나 밖에 출력 안되는 오류
    
    * todo_card_show, html 진행도 표시부터 이어서 하기
    
    * todo card show execution complete
    
    * todo juseok delete
    
    * save
    
    * todo card show completegit add .!
    
    * todo card show and auth separate

--- src/main/java/com/todorank/hlw/domain/todo_card/controller/TodoCardController.java
+++ src/main/java/com/todorank/hlw/domain/todo_card/controller/TodoCardController.java
@@ -1,5 +1,6 @@
 package com.todorank.hlw.domain.todo_card.controller;
 
+import com.todorank.hlw.domain.todo_card.entity.TodoCard;
 import com.todorank.hlw.domain.todo_card.form.TodoCardForm;
 import com.todorank.hlw.domain.todo_card.service.TodoCardService;
 import com.todorank.hlw.domain.todo_list.DTO.TodoListDTO;
@@ -7,6 +8,8 @@ import com.todorank.hlw.domain.todo_list.entity.TodoList;
 import com.todorank.hlw.domain.todo_list.service.TodoListService;
 import com.todorank.hlw.domain.todo_type_list.entity.TodoTypeList;
 import com.todorank.hlw.domain.todo_type_list.service.TodoTypeListService;
+import com.todorank.hlw.domain.user.entity.SiteUser;
+import com.todorank.hlw.domain.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,13 +17,11 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Controller
@@ -30,6 +31,7 @@ public class TodoCardController {
     private final TodoCardService todoCardService;
     private final TodoTypeListService todoTypeListService;
     private final TodoListService todoListService;
+    private final UserService userService;
 
     @GetMapping("/create")
     @PreAuthorize("isAuthenticated()")
@@ -43,6 +45,7 @@ public class TodoCardController {
         todoCardForm.setExecution(0);
         model.addAttribute("todoTypeList", typeList);
         model.addAttribute("todoListId", list_id);
+        model.addAttribute("username", todoList.getUser().getUsername());
         return "todo_card_read_create_page";
     }
 
@@ -58,11 +61,30 @@ public class TodoCardController {
         if (!todoList.getUser().getUsername().equals(principal.getName())) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "올바르지 못한 접근 입니다.");
         }
-        //TODO : execution 저장 안되는 버그
         TodoTypeList todoType = todoTypeListService.getOne(todoCardForm.getCategory());
         this.todoCardService.create(todoCardForm.getTitle(), todoCardForm.getMemo(), todoCardForm.getStartDateTime(),
                 todoCardForm.getEndDateTime(), todoCardForm.getCompletion(), todoCardForm.getExecution(),
                 todoType, todoList);
         return String.format("redirect:/todo_list/detail/%s", list_id);
+    }
+
+    @GetMapping("/detail/{id}")
+    public String todoCardDetail(TodoCardForm todoCardForm, @PathVariable(value = "id") Long cardId,
+                                 Model model, Principal principal) {
+        TodoCard todoCard = this.todoCardService.getCard(cardId);
+        List<TodoTypeList> typeList = this.todoTypeListService.getList();
+        if (todoCard == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "존재하지 않는 데이터입니다.");
+        }
+        model.addAttribute("todoTypeList", typeList);
+        model.addAttribute("username", todoCard.getTodoList().getUser().getUsername());
+        todoCardForm.setExecution(todoCard.getExecution());
+        todoCardForm.setMemo(todoCard.getMemo());
+        todoCardForm.setCategory(todoCard.getTodoTypeList().getId());
+        todoCardForm.setTitle(todoCard.getTitle());
+        todoCardForm.setCompletion(todoCard.getCompletion());
+        todoCardForm.setEndDateTime(todoCard.getEndDateTime());
+        todoCardForm.setStartDateTime(todoCard.getStartDateTime());
+        return "todo_card_read_create_page";
     }
 }
