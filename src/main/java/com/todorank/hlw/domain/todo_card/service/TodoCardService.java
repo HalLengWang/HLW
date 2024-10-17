78ca2ac8110047d3b291e6dd80359a44cac20502
LeeSamgsoo <48269505+LeeSamgsoo@users.noreply.github.com>
2024-10-17 20:17:25 +0900

    Feature/card mod del (#76)
    * test file delete
    
    * inst
    
    * todo list detail style sort
    
    * search icon delete
    
    * no err
    
    * 저장은 됨, 수정도 됨
    
    * 수정 구현 완료
    
    * card 삭제 경로 수정 script 부분
    
    * todo card delete, modify complete
    
    ---------
    
    Co-authored-by: abc <soo990414@edu.dju.ac.kr>

--- src/main/java/com/todorank/hlw/domain/todo_card/service/TodoCardService.java
+++ src/main/java/com/todorank/hlw/domain/todo_card/service/TodoCardService.java
@@ -1,6 +1,7 @@
 package com.todorank.hlw.domain.todo_card.service;
 
 import com.todorank.hlw.domain.todo_card.entity.TodoCard;
+import com.todorank.hlw.domain.todo_card.form.TodoCardForm;
 import com.todorank.hlw.domain.todo_card.repository.TodoCardRepository;
 import com.todorank.hlw.domain.todo_list.entity.TodoList;
 import com.todorank.hlw.domain.todo_type_list.entity.TodoTypeList;
@@ -11,7 +12,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,17 +20,16 @@ import java.util.List;
 public class TodoCardService {
     private final TodoCardRepository todoCardRepository;
 
-    public void create(String title, String memo, LocalDateTime startDateTime, LocalDateTime endDateTime,
-                       Boolean completion, Integer execution, TodoTypeList todoType, TodoList todoList) {
+    public void create(TodoCardForm todoCardForm, TodoTypeList type, TodoList todoList) {
         TodoCard todoCard = TodoCard.builder()
                 .todoList(todoList)
-                .title(title)
-                .memo(memo)
-                .startDateTime(startDateTime)
-                .endDateTime(endDateTime)
-                .completion(completion)
-                .execution(execution)
-                .todoTypeList(todoType)
+                .title(todoCardForm.getTitle())
+                .memo(todoCardForm.getMemo())
+                .startDateTime(todoCardForm.getStartDateTime())
+                .endDateTime(todoCardForm.getEndDateTime())
+                .completion(todoCardForm.getCompletion())
+                .execution(todoCardForm.getExecution())
+                .todoTypeList(type)
                 .build();
         this.todoCardRepository.save(todoCard);
     }
@@ -44,5 +43,22 @@ public class TodoCardService {
 
     public TodoCard getCard(Long id) {
         return this.todoCardRepository.findById(id).orElse(null);
+    }
+
+    public void modify(TodoCardForm todoCardForm, TodoCard todoCard, TodoTypeList type) {
+        TodoCard modCard = todoCard.toBuilder()
+                .title(todoCardForm.getTitle())
+                .memo(todoCardForm.getMemo())
+                .startDateTime(todoCardForm.getStartDateTime())
+                .endDateTime(todoCardForm.getEndDateTime())
+                .completion(todoCardForm.getCompletion())
+                .execution(todoCardForm.getExecution())
+                .todoTypeList(type)
+                .build();
+        this.todoCardRepository.save(modCard);
+    }
+
+    public void delete(TodoCard todoCard) {
+        this.todoCardRepository.delete(todoCard);
     }
 }
