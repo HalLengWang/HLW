d0be26a401380e5cbfccf78f77be2bbc05a1629f
Leesamgsoo <sslee3680035@gmail.com>
2024-10-22 13:48:05 +0900

    binding result err

--- src/main/java/com/todorank/hlw/domain/remembrance/controller/RemembranceController.java
+++ src/main/java/com/todorank/hlw/domain/remembrance/controller/RemembranceController.java
@@ -1,13 +1,41 @@
 package com.todorank.hlw.domain.remembrance.controller;
 
+import com.todorank.hlw.domain.remembrance.form.RemembranceForm;
 import com.todorank.hlw.domain.remembrance.service.RemembranceService;
+import com.todorank.hlw.domain.todo_list.entity.TodoList;
+import com.todorank.hlw.domain.todo_list.service.TodoListService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/remembrance")
 public class RemembranceController {
     private final RemembranceService remembranceService;
+    private final TodoListService todoListService;
+
+    @PostMapping("/create/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public String createRemembrance(@Valid RemembranceForm remembranceForm, BindingResult bindingResult,
+                                    @PathVariable("id") Long listId, Principal principal) {
+        if (bindingResult.hasErrors()) {
+            return "redirect:/todo_list/detail/" + listId;
+        }
+        TodoList todoList = this.todoListService.getTodoList(listId);
+        if (todoList == null || !todoList.getUser().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "올바르지 못한 접근입니다.");
+        }
+        this.remembranceService.create(remembranceForm, todoList);
+        return "redirect:/todo_list/detail/" + listId;
+    }
 }
--- src/main/java/com/todorank/hlw/domain/remembrance/service/RemembranceService.java
+++ src/main/java/com/todorank/hlw/domain/remembrance/service/RemembranceService.java
@@ -1,6 +1,9 @@
 package com.todorank.hlw.domain.remembrance.service;
 
+import com.todorank.hlw.domain.remembrance.entity.Remembrance;
+import com.todorank.hlw.domain.remembrance.form.RemembranceForm;
 import com.todorank.hlw.domain.remembrance.repository.RemembranceRepository;
+import com.todorank.hlw.domain.todo_list.entity.TodoList;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +11,14 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class RemembranceService {
     private final RemembranceRepository remembranceRepository;
+
+    public void create (RemembranceForm remembranceForm, TodoList todoList) {
+        Remembrance remembrance = Remembrance.builder()
+                .title(remembranceForm.getTitle())
+                .content(remembranceForm.getContent())
+                .isPublic(remembranceForm.getIsPublic())
+                .todoList(todoList)
+                .build();
+        this.remembranceRepository.save(remembrance);
+    }
 }
