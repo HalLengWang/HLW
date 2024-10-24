d784b0c040834bddd3f902e739380e9e089b6dc6
starlike3279 <69036689+starlike3279@users.noreply.github.com>
2024-10-24 17:23:26 +0900

    Update TodoCardController.java (#112)

--- src/main/java/com/todorank/hlw/domain/todo_card/controller/TodoCardController.java
+++ src/main/java/com/todorank/hlw/domain/todo_card/controller/TodoCardController.java
@@ -17,11 +17,14 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.security.Principal;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
